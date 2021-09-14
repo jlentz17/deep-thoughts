a08ac01cd233c6bfdb6fbd5ac42448c8350ada3e
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-09-14 11:03:17 -0700

    added concurrency

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import Home from "./pages/Home";
 
 function App() {
   const httpLink = createHttpLink({
-    uri: "http://localhost:3001/graphql",
+    uri: "/graphql",
   });
 
   const client = new ApolloClient({
--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
 const express = require("express");
+const path = require("path");
 // import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
-const authMiddleware = require("./utils/auth")
+const authMiddleware = require("./utils/auth");
 const PORT = process.env.PORT || 3001;
 const app = express();
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
+  context: authMiddleware,
 });
 
 // integrate our Apollo server with te Express application as middleware
@@ -21,6 +22,14 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")))
+}
+
+app.get("*", (req,res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+})
 db.once("open", () => {
   app.listen(PORT, () => {
     // log where we can go to test our GQL API
